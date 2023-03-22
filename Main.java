c3a748e5ee7f99a54a972da033d4482597616dea
Gudykson <127319413+Gudykson@users.noreply.github.com>
2023-03-22 14:06:13 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main
+{
+  public static void main(String[] args) {
+  double a = 2;
+  double b = 8;
+  double suma = a+b;
+  double roz = a-b;
+  double iloczyn = a*b;
+  double iloraz = a/b;
+  System.out.print("suma wynosi " + suma + " roznica wynosi " + roz + " iloczyn wynosi " + iloczyn  + " iloraz wynosi " + iloraz );
+  }
+}
